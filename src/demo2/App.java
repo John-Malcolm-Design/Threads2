f7ad8fd0ed7b9a7802be65309fe5865708116dbe
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2015-11-28 21:50:49 +0000

    My First Commit

---
+++ src/demo2/App.java
@@ -0,0 +1,49 @@
+package demo2;
+
+public class App {
+	
+	private int count = 0;
+	
+	private synchronized void incrementCount() {
+		count++;
+		
+	}
+
+	public static void main(String[] args) {
+		App app = new App();
+		app.doWork();
+	}
+
+	private void doWork() {
+		Thread t1 = new Thread(new Runnable(){
+			public void run(){
+				for (int i = 0; i < 10000; i++) {
+					incrementCount();
+				}
+			}
+
+		});
+		
+		Thread t2 = new Thread(new Runnable(){
+			public void run(){
+				for (int i = 0; i < 10000; i++) {
+					incrementCount();
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Count is:" + count);
+		
+	}
+}
